f7b092498d13f15adafc6d60ba3dcd682a417ac9
Harsimran jeet Singh <harsimranjeetsingh4@gmail.com>
2026-02-07 00:27:12 -0500

    Create HeroSection component

---
+++ src/components/sections/HeroSection.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './HeroSection.css';
+
+const HeroSection = () => {
+    return (
+        <div className="hero-container">
+            <h1>Welcome to Our Website</h1>
+            <p>Your journey to excellence starts here.</p>
+            <button className="btn">Get Started</button>
+        </div>
+    );
+};
+
+export default HeroSection;
